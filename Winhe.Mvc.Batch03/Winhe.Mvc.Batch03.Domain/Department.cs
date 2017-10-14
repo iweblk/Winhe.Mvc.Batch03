fbdaab75d865fd7588a654a90ac156a02ed5386b
Innovative Web Solutions (iWeb Solutions) <iwebs.lk@gmail.com>
2017-10-14 10:58:56 +0530

    Properties added

--- Winhe.Mvc.Batch03/Winhe.Mvc.Batch03.Domain/Department.cs
+++ Winhe.Mvc.Batch03/Winhe.Mvc.Batch03.Domain/Department.cs
@@ -8,5 +8,9 @@ namespace Winhe.Mvc.Batch03.Domain
 {
     public class Department
     {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public int Telephone { get; set; }
+
     }
 }
